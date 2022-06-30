f16cf2eee016e690d12611112258e8f7fcb1d163
tasse00 <tasse_00@163.com>
2022-06-30 13:06:50 +0800

    script: deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,56 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { utils } from "ethers";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [account] = await ethers.getSigners();
+  console.log(`use account: ${account.address}`);
+  const FindTrumanToken = await ethers.getContractFactory("FindTrumanToken");
+  const ftt = await upgrades.deployProxy(FindTrumanToken, [], {
+    kind: "uups",
+  });
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await ftt.deployed();
+  console.log(`FTT deployed at: ${ftt.address}`);
 
-  await greeter.deployed();
+  // console.log(`use account: ${account.address}`);
+  const FindTrumanAchievement = await ethers.getContractFactory(
+    "FindTrumanAchievement"
+  );
+  const fta = await upgrades.deployProxy(FindTrumanAchievement, [], {
+    kind: "uups",
+  });
 
-  console.log("Greeter deployed to:", greeter.address);
+  await fta.deployed();
+  console.log(`FTA deployed at: ${fta.address}`);
+
+  const FindTrumanCocreation = await ethers.getContractFactory(
+    "FindTrumanCocreation"
+  );
+  const instance = await upgrades.deployProxy(
+    FindTrumanCocreation,
+    [fta.address, ftt.address],
+    {
+      kind: "uups",
+    }
+  );
+
+  await instance.deployed();
+  console.log(`FTC deployed at: ${instance.address}`);
+
+  const MINTER_ROLE = utils.keccak256(utils.toUtf8Bytes("MINTER_ROLE"));
+
+  for (const ins of [fta, ftt]) {
+    console.log(
+      `[${ins.address}] grant MINTER_ROLE to ${instance.address} ...`
+    );
+    const tx = await ins.grantRole(MINTER_ROLE, instance.address);
+    await tx.wait();
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
